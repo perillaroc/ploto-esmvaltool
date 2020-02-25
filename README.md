d1c4b38aeaf8510770b3d10d52c560552d19fcf5
perillaroc <perillaroc@gmail.com>
2020-02-25 18:51:30 +0800

    :tada: initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ploto-esmvaltool
+
+An extension for ESMValTool.
+
+## LICENSE
+
+Copyright 2020, perillaroc at nwpc-oper.
+
+`ploto-esmvaltool` is licensed under [GPL-3.0](./LICENSE.md).
---
+++ setup.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+import io
+import re
+
+with io.open("ploto_esmvaltool/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setup(
+    name='ploto-esmvaltool',
+
+    version=version,
+
+    description='Ploto-esmvaltool project.',
+    long_description=__doc__,
+
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+
+    include_package_data=True,
+
+    package_data={
+        '': ['*.ncl'],
+    },
+
+    zip_safe=False,
+
+    install_requires=[
+        'click',
+        'pyyaml',
+        'loguru',
+    ],
+
+    extras_require={
+        'test': [],
+    }
+)
