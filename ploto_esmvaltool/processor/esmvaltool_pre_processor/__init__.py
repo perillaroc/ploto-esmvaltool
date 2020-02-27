8870e938bbd9f47d4ca380b78b3241955af9331c
perillaroc <perillaroc@gmail.com>
2020-02-27 20:29:22 +0800

    :construction: write config.yaml into work dir and load config using read_config_user_file.

--- ploto_esmvaltool/processor/esmvaltool_pre_processor/__init__.py
+++ ploto_esmvaltool/processor/esmvaltool_pre_processor/__init__.py
@@ -1,16 +1,12 @@
 from copy import deepcopy
-import os
-import datetime
+from pathlib import Path
 
 from loguru import logger
+import yaml
 
 from esmvalcore._config import (
     configure_logging,
-    CFG_USER,
-    CFG,
-    read_config_developer_file,
-    read_cmor_tables,
-    _normalize_path,
+    read_config_user_file,
 )
 from esmvalcore._recipe import (
     get_recipe_provenance,
@@ -57,8 +53,12 @@ def run_processor(
 
     entity = get_recipe_provenance(doc, "recipe.yml")
 
-    # get from _config.read_config_user_file function
-    config_user = get_config_user(task["config"], task["recipe_name"])
+    # config
+    config_file_path = Path(work_dir, "config.yaml")
+    with open(config_file_path, "w") as f:
+        yaml.safe_dump(task["config"], f)
+
+    config_user = read_config_user_file(config_file_path, task["recipe_name"])
 
     config_user['skip-nonexistent'] = False
     config_user['diagnostics'] = {}
@@ -80,67 +80,3 @@ def run_processor(
 
     logger.info(f"output files: {output_files}")
     logger.info("running processor done: esmvaltool_pre_processor")
-
-
-def get_config_user(cfg: dict, recipe_name: str):
-    # set defaults
-    defaults = {
-        'write_plots': True,
-        'write_netcdf': True,
-        'compress_netcdf': False,
-        'exit_on_warning': False,
-        'max_data_filesize': 100,
-        'output_file_type': 'ps',
-        'output_dir': './output_dir',
-        'auxiliary_data_dir': './auxiliary_data',
-        'save_intermediary_cubes': False,
-        'remove_preproc_dir': False,
-        'max_parallel_tasks': 1,
-        'run_diagnostic': True,
-        'profile_diagnostic': False,
-        'config_developer_file': None,
-        'drs': {},
-    }
-
-    for key in defaults:
-        if key not in cfg:
-            logger.info(
-                "No %s specification in config file, "
-                "defaulting to %s", key, defaults[key])
-            cfg[key] = defaults[key]
-
-    cfg['output_dir'] = _normalize_path(cfg['output_dir'])
-    cfg['auxiliary_data_dir'] = _normalize_path(cfg['auxiliary_data_dir'])
-
-    cfg['config_developer_file'] = _normalize_path(
-        cfg['config_developer_file'])
-
-    for key in cfg['rootpath']:
-        root = cfg['rootpath'][key]
-        if isinstance(root, str):
-            cfg['rootpath'][key] = [_normalize_path(root)]
-        else:
-            cfg['rootpath'][key] = [_normalize_path(path) for path in root]
-
-    # insert a directory date_time_recipe_usertag in the output paths
-    now = datetime.datetime.utcnow().strftime("%Y%m%d_%H%M%S")
-    new_subdir = '_'.join((recipe_name, now))
-    cfg['output_dir'] = os.path.join(cfg['output_dir'], new_subdir)
-
-    # create subdirectories
-    cfg['preproc_dir'] = os.path.join(cfg['output_dir'], 'preproc')
-    cfg['work_dir'] = os.path.join(cfg['output_dir'], 'work')
-    cfg['plot_dir'] = os.path.join(cfg['output_dir'], 'plots')
-    cfg['run_dir'] = os.path.join(cfg['output_dir'], 'run')
-
-    # Save user configuration in global variable
-    for key, value in cfg.items():
-        CFG_USER[key] = value
-
-    # Read developer configuration file
-    cfg_developer = read_config_developer_file(cfg['config_developer_file'])
-    for key, value in cfg_developer.items():
-        CFG[key] = value
-    read_cmor_tables(CFG)
-
-    return cfg
