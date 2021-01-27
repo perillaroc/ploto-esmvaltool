425adb1d32ebc979f4bc85c59015fd5fbf779538
perillaroc <perillaroc@gmail.com>
2021-01-27 16:32:43 +0800

    :art: add more variables.

--- test/recipes/atmos/diurnal_temperature_index/processor/combine_metadata.py
+++ test/recipes/atmos/diurnal_temperature_index/processor/combine_metadata.py
@@ -4,27 +4,31 @@ from ploto_esmvaltool.processor.esmvaltool_util_processor import run_processor
 def main():
     work_dir = "/home/hujk/ploto/esmvaltool/cases/case2/ploto/processor"
 
-    short_name = "tasmax"
+    short_names = [
+        "tasmax",
+        "tasmin",
+    ]
 
     exps = [
         "historical",
         "ssp119"
     ]
 
-    task = {
+    tasks = [{
         "util_type": "combine_metadata",
         "metadata_files": [
             f"{work_dir}/preproc/{exp}/{short_name}/metadata.yml"
             for exp in exps
         ],
         "output_directory": f"{work_dir}/preproc/{short_name}"
-    }
+    } for short_name in short_names ]
 
-    run_processor(
-        task=task,
-        work_dir=work_dir,
-        config={}
-    )
+    for task in tasks:
+        run_processor(
+            task=task,
+            work_dir=work_dir,
+            config={}
+        )
 
 
 if __name__ == "__main__":
