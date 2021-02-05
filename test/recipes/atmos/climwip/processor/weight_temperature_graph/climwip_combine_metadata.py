69136824d6f904640019fe0de49e0a8ed7ed740c
perillaroc <perillaroc@gmail.com>
2021-02-05 14:18:05 +0800

    :white_check_mark: add combine metadata for temperature graph.

---
+++ test/recipes/atmos/climwip/processor/weight_temperature_graph/climwip_combine_metadata.py
@@ -0,0 +1,35 @@
+from ploto_esmvaltool.processor.esmvaltool_util_processor import run_processor
+
+
+base_dir = "/home/hujk/ploto/esmvaltool/cases/case105/ploto"
+
+def get_task(work_dir, short_name):
+    task = {
+        "util_type": "combine_metadata",
+        "metadata_files": [
+            f"{base_dir}/processor/graph/preproc/FGOALS-g3/{short_name}/metadata.yml",
+            f"{base_dir}/processor/graph/preproc/CAMS-CSM1-0/{short_name}/metadata.yml",
+        ],
+        "output_directory": f"{work_dir}/graph/preproc/{short_name}"
+    }
+    return task
+
+
+
+def main():
+    work_dir = f"{base_dir}/processor"
+
+    tasks = [
+        get_task(work_dir, short_name) for short_name in ["tas"]
+    ]
+
+    for task in tasks:
+        run_processor(
+            task=task,
+            work_dir=work_dir,
+            config={}
+        )
+
+
+if __name__ == "__main__":
+    main()
