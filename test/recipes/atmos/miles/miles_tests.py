442af8c290f149215b148c991fd4b6e4a9bdf8c6
perillaroc <perillaroc@gmail.com>
2021-02-09 15:36:46 +0800

    :white_check_mark: recycle miles test to use recipe script.

--- test/recipes/atmos/miles/miles_tests.py
+++ test/recipes/atmos/miles/miles_tests.py
@@ -1,283 +1,225 @@
+import itertools
+
 from ploto_esmvaltool.plotter.esmvaltool_diag_plotter.atmosphere.miles import (
     generate_default_plot_task,
     generate_default_operations,
 )
 from ploto.run import run_ploto
 
-
-short_name = "zg"
-
-start_year = 1980
-end_year = 1985
-
-common_dataset = {
-    "frequency": "day",
-    "mip": "day",
-
-    "start_year": start_year,
-    "end_year": end_year,
-}
-
-# variable
-
-variable = {
-    "short_name": short_name,
-    "variable_group": short_name,
-
-    "preprocessor": "preproc",
-    "reference_dataset": "ERA-Interim",  # *****************
-}
-
-# processor
-
-processor_settings = {
-    "extract_region": {
-        "start_longitude": 0,
-        "end_longitude": 360,
-        "start_latitude": -1.25,
-        "end_latitude": 90,
-    },
-    "extract_levels": {
-        "levels": 50000,
-        "scheme": "linear"
-    },
-    "regrid": {
-        "target_grid": "2.5x2.5",
-        "scheme": "linear_extrapolate"
-    }
-}
-
-# datasets
-
-exp = "historical"
-exp_dataset = {
-    "dataset": "FGOALS-g3",
-    "project": "CMIP6",
-    "exp": exp,
-    "ensemble": "r1i1p1f1",
-    "grid": "gn",
-    "type": "exp",
-}
-
-reanaly_dataset = {
-    "dataset": "ERA-Interim",
-    "project": "OBS6",
-    "type": "reanaly",
-    "version": 1,
-    "tier": 3,
-}
-
-# config
-
-data_path = {
-    "CMIP6": [
-        "/home/hujk/clusterfs/wangdp/data/CMIP6"
-    ],
-    "OBS6": [
-        # "/home/hujk/clusterfs/wangdp/data/obs"
-        "/data/brick/b2/OBS/esmvaltool_output/cmorize_obs_20210119_071639"
-    ],
-}
-
-plot_config = {
-    "log_level": "info",
-    "write_netcdf": True,
-    "write_plots": True,
-    "output_file_type": "png",
-    "profile_diagnostic": False,
-    "auxiliary_data_dir": "/home/hujk/ploto/esmvaltool/cases/case1/case1.2/auxiliary_data"
-}
+from test.recipes.atmos.miles import recipe as miles_recipe
+from test.recipes.atmos.miles import config as miles_config
 
 
-def get_fetcher_steps():
-    dataset = {
+def get_fetcher(
+    exp_dataset,
+    variable,
+    diagnostic_name
+):
+    combined_dataset = {
         **exp_dataset,
-        **common_dataset
+        **variable
     }
 
     variables = [
-        {
-            "short_name": short_name,
-        }
+        variable
     ]
 
     task = {
-        "dataset": dataset,
+        "dataset": combined_dataset,
         "variables": variables,
-        "data_path": data_path,
+        "data_path": miles_config.data_path,
 
-        "output_directory": "{work_dir}" + f"/preproc/{dataset['exp']}/{short_name}",
+        "output_directory": "{work_dir}" + f"/{diagnostic_name}/fetcher/preproc/{combined_dataset['dataset']}/{combined_dataset['variable_group']}",
         "output_data_source_file": "data_source.yml",
 
         "step_type": "fetcher",
         "type": "ploto_esmvaltool.fetcher.esmvalcore_fetcher",
     }
+    return task
 
-    era_dataset = {
-        **reanaly_dataset,
-        **common_dataset
-    }
 
-    era_task = {
-        "dataset": era_dataset,
-        "variables": variables,
-        "data_path": data_path,
+def get_fetcher_steps():
+    exp_datasets = miles_recipe.exp_datasets
+    variables = miles_recipe.variables
 
-        "output_directory": "{work_dir}" + f"/preproc/{era_dataset['dataset']}/{short_name}",
-        "output_data_source_file": "data_source.yml",
-
-        "step_type": "fetcher",
-        "type": "ploto_esmvaltool.fetcher.esmvalcore_fetcher",
-    }
-
-    return [
-        task,
-        era_task
+    tasks = [
+        {
+            "exp_dataset": d,
+            "variable": v,
+            "diagnostic_name": "miles_block"
+        }
+        for d, v in itertools.product(exp_datasets, variables)
     ]
+    return [get_fetcher(**task) for task in tasks]
 
 
-def get_processor_steps():
+def get_processor(
+        exp_dataset,
+        variable,
+        diagnostic_name
+):
     operations = generate_default_operations()
 
-    recipe_dataset_index = 0
-    alias = "historical"
-
-    dataset = {
+    combined_dataset = {
         **exp_dataset,
-        **common_dataset,
+        **variable
     }
 
-    diagnostic_dataset = {
-        "recipe_dataset_index": recipe_dataset_index,
-        "alias": alias,
+    diag_dataset = {
         "modeling_realm": [
             "atmos"
-        ],
-        "reference_dataset": "ERA-Interim"
+        ]
     }
 
-    diagnostic = {
+    variable = variable
+
+    diag = {
         "diagnostic": "diurnal_temperature_indicator",
     }
 
+    settings = {
+        "extract_region": {
+            "start_longitude": 0,
+            "end_longitude": 360,
+            "start_latitude": -1.25,
+            "end_latitude": 90,
+        },
+        "extract_levels": {
+            "levels": 50000,
+            "scheme": "linear"
+        },
+        "regrid": {
+            "target_grid": "2.5x2.5",
+            "scheme": "linear_extrapolate"
+        }
+    }
+
     task = {
-        "input_data_source_file": "{work_dir}" + f"/preproc/{dataset['exp']}/{variable['short_name']}/data_source.yml",
+        "input_data_source_file": "{work_dir}" + f"/{diagnostic_name}/fetcher/preproc/{combined_dataset['dataset']}/{combined_dataset['variable_group']}/data_source.yml",
         # output
-        "output_directory": "{work_dir}" + f"/preproc/{dataset['exp']}/{variable['short_name']}",
+        "output_directory": "{work_dir}" + f"/{diagnostic_name}/processor/preproc/{combined_dataset['dataset']}/{combined_dataset['variable_group']}",
 
         # operations
         "operations": operations,
 
-        "dataset": dataset,
-        "diagnostic_dataset": diagnostic_dataset,
+        "dataset": combined_dataset,
+        "diagnostic_dataset": diag_dataset,
         "variable": variable,
-        "diagnostic": diagnostic,
-        "settings": processor_settings,
+        "diagnostic": diag,
+        "settings": settings,
 
         "step_type": "processor",
         "type": "ploto_esmvaltool.processor.esmvalcore_pre_processor",
     }
+    return task
 
-    recipe_dataset_index = 1
-    alias = "OBS6"
 
-    era_dataset = {
-        **reanaly_dataset,
-        **common_dataset
-    }
+def get_combine_processor(
+        exp_datasets,
+        variables,
+        diagnostic_name
+):
+    tasks = [
+        {
+            "util_type": "combine_metadata",
+            "metadata_files": [
+                "{work_dir}" + f"/{diagnostic_name}/processor/preproc/{d['dataset']}/{v['variable_group']}/metadata.yml"
+                for d, v in itertools.product(exp_datasets, variables)
+            ],
+            "output_directory": "{work_dir}" + f"/{diagnostic_name}/processor/preproc/",
 
-    diagnostic_dataset = {
-        "recipe_dataset_index": recipe_dataset_index,
-        "alias": alias,
-        "modeling_realm": [
-            "atmos"
-        ],
-        "reference_dataset": "ERA-Interim"
-    }
-
-    era_task = {
-        "input_data_source_file": "{work_dir}" + f"/preproc/{era_dataset['dataset']}/{variable['short_name']}/data_source.yml",
-        # output
-        "output_directory": "{work_dir}" + f"/preproc/{era_dataset['dataset']}/{variable['short_name']}",
-
-        # operations
-        "operations": operations,
-
-        "dataset": era_dataset,
-        "diagnostic_dataset": diagnostic_dataset,
-        "variable": variable,
-        "diagnostic": diagnostic,
-        "settings": processor_settings,
-
-        "step_type": "processor",
-        "type": "ploto_esmvaltool.processor.esmvalcore_pre_processor",
-    }
-
-    combine_task = {
-        "util_type": "combine_metadata",
-        "metadata_files": [
-            "{work_dir}" + f"/preproc/{dataset['exp']}/{variable['short_name']}/metadata.yml",
-            "{work_dir}" + f"/preproc/{era_dataset['dataset']}/{variable['short_name']}/metadata.yml"
-        ],
-        "output_directory": "{work_dir}/preproc/",
-
-        "step_type": "processor",
-        "type": "ploto_esmvaltool.processor.esmvaltool_util_processor",
-    }
-
-    return [
-        task,
-        era_task,
-        combine_task
+            "step_type": "processor",
+            "type": "ploto_esmvaltool.processor.esmvaltool_util_processor",
+        }
     ]
+    return tasks
+
+
+def get_processor_steps():
+    steps = []
+    exp_datasets = miles_recipe.exp_datasets
+    datasets = [
+        {
+            **d,
+            "recipe_dataset_index": index,
+            "alias": d["dataset"]
+        }
+        for index, d in enumerate(exp_datasets)
+    ]
+
+    variables = miles_recipe.variables
+
+    tasks = [
+        {
+            "exp_dataset": d,
+            "variable": v,
+            "diagnostic_name": "miles_block",
+        }
+        for d, v in itertools.product(datasets, variables)
+    ]
+    steps.extend([get_processor(**task) for task in tasks])
+
+    steps.extend(get_combine_processor(datasets, variables, "miles_block"))
+
+    return steps
 
 
 def get_plotter_steps():
-    plot_task = generate_default_plot_task(name="miles_block")
+    steps = []
+    diagnostic_name = "miles_block"
+
+    plot_task = generate_default_plot_task(name=diagnostic_name)
     plot_task["diagnostic_script"]["settings"]["seasons"] = "DJF"
 
-    block_task = {
-        **plot_task,
-        "config": plot_config,
-        "input_files": [
-            "{work_dir}/preproc/metadata.yml"
-        ],
-
+    task = {
         "step_type": "plotter",
         "type": "ploto_esmvaltool.plotter.esmvaltool_diag_plotter",
-    }
 
-    plot_task = generate_default_plot_task(name="miles_eof")
+        **plot_task,
+        "config": miles_config.plot_config,
+        "input_files": [
+            "{work_dir}" + f"/{diagnostic_name}/processor/preproc/metadata.yml"
+        ],
+    }
+    steps.append(task)
+
+    diagnostic_name = "miles_eof"
+    data_diagnostic_name = "miles_block"
+
+    plot_task = generate_default_plot_task(name=diagnostic_name)
     plot_task["diagnostic_script"]["settings"]["seasons"] = "DJF"
     plot_task["diagnostic_script"]["settings"]["teles"] = "NAO"
 
-    eof_task = {
+    task = {
         "step_type": "plotter",
         "type": "ploto_esmvaltool.plotter.esmvaltool_diag_plotter",
+
         **plot_task,
-        "config": plot_config,
+        "config": miles_config.plot_config,
         "input_files": [
-            "{work_dir}/preproc/metadata.yml"
+            "{work_dir}" + f"/{data_diagnostic_name}/processor/preproc/metadata.yml"
         ],
     }
+    steps.append(task)
 
-    plot_task = generate_default_plot_task(name="miles_regimes")
+    diagnostic_name = "miles_regimes"
+    data_diagnostic_name = "miles_block"
 
-    regime_task = {
+    plot_task = generate_default_plot_task(name=diagnostic_name)
+
+    task = {
         "step_type": "plotter",
         "type": "ploto_esmvaltool.plotter.esmvaltool_diag_plotter",
+
         **plot_task,
-        "config": plot_config,
+        "config": miles_config.plot_config,
         "input_files": [
-            "{work_dir}/preproc/metadata.yml"
+            "{work_dir}" + f"/{data_diagnostic_name}/processor/preproc/metadata.yml"
         ],
     }
+    steps.append(task)
 
-    return [
-        block_task,
-        eof_task,
-        regime_task,
-    ]
+    return steps
 
 
 def run_miles_block():
@@ -286,23 +228,7 @@ def run_miles_block():
     steps.extend(get_processor_steps())
     steps.extend(get_plotter_steps())
 
-    config = {
-        "esmvaltool": {
-            "executables": {
-                "py": "/home/hujk/anaconda3/envs/wangdp-esm/bin/python",
-                "r": "/home/hujk/anaconda3/envs/wangdp-esm/bin/Rscript"
-            },
-            "recipes": {
-                "base": "/home/hujk/ploto/esmvaltool/study/esmvaltool/ESMValTool/esmvaltool/recipes",
-            },
-            "diag_scripts": {
-                "base": "/home/hujk/ploto/esmvaltool/study/esmvaltool/ESMValTool/esmvaltool/diag_scripts",
-            },
-        },
-        "base": {
-            "run_base_dir": "/home/hujk/ploto/esmvaltool/cases/case3/run"
-        }
-    }
+    config = miles_config.config
 
     run_ploto({
         "data": {
