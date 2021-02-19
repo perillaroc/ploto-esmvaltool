d0d112b378fe0cadceff87b84b1f1cd4e7dc2279
perillaroc <perillaroc@gmail.com>
2021-02-19 09:05:13 +0800

    :construction: add fetcher for one variable.

--- test/recipes/atmos/deangelis/recipe.py
+++ test/recipes/atmos/deangelis/recipe.py
@@ -1,3 +1,77 @@
+exp_datasets = [
+    {
+        "dataset": "FGOALS-g3",
+        "project": "CMIP6",
+        "exp": "piControl",
+        "start_year": 501,
+        "end_year": 550
+    },
+    {
+        "dataset": "FGOALS-g3",
+        "project": "CMIP6",
+        "exp": "ssp585",
+        "start_year": 2090,
+        "end_year": 2099
+    },
+    {
+        "dataset": "FGOALS-g3",
+        "project": "CMIP6",
+        "exp": "abrupt-4xCO2",
+        "start_year": 501,
+        "end_year": 550
+    },
+]
+
+
+spatial_mean_variable = {
+    "preprocessor": "spatial_mean",
+
+    "project": "CMIP6",
+    "ensemble": "r1i1p1f1",
+    "mip": "Amon",
+    "grid": "gn",
+    "frequency": "mon",
+}
+
+
+variables = [
+    {
+        "short_name": "rlnst",
+        "variable_group": "rlnst",
+
+        **spatial_mean_variable,
+
+        "derive": True,
+        "force_derivation": False,
+    },
+    {
+        "short_name": "rsnst",
+        "variable_group": "rsnst",
+
+        **spatial_mean_variable,
+
+        "derive": True,
+        "force_derivation": False,
+    },
+    {
+        "short_name": "lvp",
+        "variable_group": "lvp",
+
+        **spatial_mean_variable,
+
+        "derive": True,
+        "force_derivation": False,
+    },
+    {
+        "short_name": "hfss",
+        "variable_group": "hfss",
+
+        **spatial_mean_variable,
+
+        "derive": False,
+        "force_derivation": False,
+    },
+]
 
 
 processor_settings = {
