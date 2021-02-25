644cf10a0b94cadb8aba15d019397e0e471f7638
perillaroc <perillaroc@gmail.com>
2021-02-25 16:09:26 +0800

    :sparkles: add institute and activity in add_variable_info.

--- ploto_esmvaltool/util/esmvaltool/__init__.py
+++ ploto_esmvaltool/util/esmvaltool/__init__.py
@@ -1,10 +1,23 @@
 import typing
 
 from esmvalcore._recipe import _add_cmor_info
+from esmvalcore._config import (
+    get_institutes,
+    get_activity,
+)
 
 
 def add_variable_info(variable: typing.Dict, override: bool=False):
     _add_cmor_info(variable, override)
+
+    if 'institute' not in variable:
+        institute = get_institutes(variable)
+        if institute:
+            variable['institute'] = institute
+    if 'activity' not in variable:
+        activity = get_activity(variable)
+        if activity:
+            variable['activity'] = activity
 
 
 def combine_variable(variable: typing.Dict, dataset: typing.Dict):
