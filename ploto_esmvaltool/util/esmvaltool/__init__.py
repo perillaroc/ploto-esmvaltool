bab9360c50e8100099424c93ddad05436413ba50
perillaroc <perillaroc@gmail.com>
2021-03-04 16:24:26 +0800

    :recycle: extract functions into variable.py.

--- ploto_esmvaltool/util/esmvaltool/__init__.py
+++ ploto_esmvaltool/util/esmvaltool/__init__.py
@@ -1,32 +1,5 @@
-import typing
-
-from esmvalcore._recipe import _add_cmor_info
-from esmvalcore._config import (
-    get_institutes,
-    get_activity,
+from .variable import (
+    combine_variable,
+    add_variable_info,
 )
 
-
-def add_variable_info(variable: typing.Dict, override: bool=False):
-    _add_cmor_info(variable, override)
-
-    if 'institute' not in variable:
-        institute = get_institutes(variable)
-        if institute:
-            variable['institute'] = institute
-    if 'activity' not in variable:
-        activity = get_activity(variable)
-        if activity:
-            variable['activity'] = activity
-
-
-def combine_variable(variable: typing.Dict, dataset: typing.Dict):
-    return {
-        **variable,
-        **dataset,
-    }
-
-
-def replace_variable_tag(item: str, variable: typing.Dict) -> str:
-    r = item.format(**variable)
-    return r
---
+++ ploto_esmvaltool/util/esmvaltool/variable.py
@@ -0,0 +1,35 @@
+import typing
+
+from esmvalcore._config import get_institutes, get_activity
+from esmvalcore._recipe import _add_cmor_info
+
+
+def add_variable_info(
+        variable: typing.Dict,
+        override: bool=False
+):
+    _add_cmor_info(variable, override)
+
+    if 'institute' not in variable:
+        institute = get_institutes(variable)
+        if institute:
+            variable['institute'] = institute
+    if 'activity' not in variable:
+        activity = get_activity(variable)
+        if activity:
+            variable['activity'] = activity
+
+
+def combine_variable(
+        variable: typing.Dict,
+        dataset: typing.Dict
+):
+    return {
+        **variable,
+        **dataset,
+    }
+
+
+def replace_variable_tag(item: str, variable: typing.Dict) -> str:
+    r = item.format(**variable)
+    return r
